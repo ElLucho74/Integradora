24f1fff4b8b66ae5ff390e9523e4a726a2ec822b
martharoman <martha.rxman@gmail.com>
2023-03-07 22:26:26 -0600

    v2

--- src/views/index-sections/Navbars.js
+++ src/views/index-sections/Navbars.js
@@ -26,12 +26,17 @@ function Navbars() {
       <div className="section section-navbars">
         <Container id="menu-dropdown">
           <Row>
-            <Col /* md="6" */>
+            <Col md="6" className="mt-5">
+              <br></br>
               <h4>Acerca de Nosotros</h4>
                 <Container style={{textAlign: "justify"}}>
                  <h5>Somos un equipo de la carrera de Tecnologías de la Información (TI) de la Universidad Tecnológica de Durango encargada de diseñar y crear una estación meteorológica para darle solución a un problema presentado en el Instituto Tecnológico del Valle del Guadiana (ITVG).</h5>
                 </Container>
-              
+            </Col>
+            <Col className="mt-5">
+              <br></br>
+            <img src={require("assets/img/guapitos.jpeg")}
+                ></img>
             </Col>
            
           </Row>
