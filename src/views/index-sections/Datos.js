d3a238129eaf0821bfa2d8d30d6277828d84f708
miguelmoranv <miguelonchas001@gmail.com>
2023-05-15 03:39:54 -0600

    exportFront

--- src/views/index-sections/Datos.js
+++ src/views/index-sections/Datos.js
@@ -1,6 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import ExportExcel from 'react-export-excel';
 
+import { Alert } from 'reactstrap';
+
+import {
+    Button,
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Form,
+    Input,
+    InputGroupAddon,
+    InputGroupText,
+    InputGroup,
+    Container,
+    Col
+  } from "reactstrap";
+
 const ExcelFile = ExportExcel.ExcelFile;
 const ExcelSheet = ExportExcel.ExcelSheet;
 const ExcelColumn = ExportExcel.ExcelColumn;
@@ -17,19 +34,27 @@ function Datos() {
     }, []);
 
     return ( 
-    <>
-        <ExcelFile element={<button>Exportar a Excel</button>} filename="Registros 30 dias" >
-                <ExcelSheet data={datos} name="Registros 30 dias">
-                    <ExcelColumn label="Dispositivo" value="Dispositivo"/>
-                    <ExcelColumn label="Velocidad_viento" value="Velocidad_viento"/>
-                    <ExcelColumn label="Temperatura" value="Temperatura"/>
-                    <ExcelColumn label="Humedad_atm" value="Humedad_atm"/>
-                    <ExcelColumn label="Humedad_cultivo" value="Humedad_cultivo"/>
-                    <ExcelColumn label="Date" value="Date"/>
-                    <ExcelColumn label="Hour" value="Hour"/>
-                </ExcelSheet>
-            </ExcelFile>
-    </> );
+    <div align="center">
+        <Alert color="primary">
+            Bienvenido
+</Alert>
+        <p>Registro de los ultimos 30 dias {''} <i className="now-ui-icons arrows-1_minimal-right"></i> {''}
+            <ExcelFile element={<Button className="btn-round" size="lg" color="success" outline> <i className="now-ui-icons arrows-1_cloud-download-93"></i> Exportar a Excel</Button>} filename="Registros 30 dias" >
+                    <ExcelSheet data={datos} name="Registros 30 dias">
+                        <ExcelColumn label="Dispositivo" value="Dispositivo"/>
+                        <ExcelColumn label="Velocidad_viento" value="Velocidad_viento"/>
+                        <ExcelColumn label="Temperatura" value="Temperatura"/>
+                        <ExcelColumn label="Humedad_atm" value="Humedad_atm"/>
+                        <ExcelColumn label="Humedad_cultivo" value="Humedad_cultivo"/>
+                        <ExcelColumn label="Date" value="Date"/>
+                        <ExcelColumn label="Hour" value="Hour"/>
+                    </ExcelSheet>
+                </ExcelFile>
+                {' '}
+                18 mb
+                </p>
+    </div> 
+    );
 }
 
 export default Datos;
